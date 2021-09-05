e23280ab103238a8028ede1091a8e821ce797485
bjvanbemmel <97085143@st.deltion.nl>
2021-09-05 16:00:31 +0200

    removed responsiveness, changed left width

--- src/js/projects-dropdown.js
+++ src/js/projects-dropdown.js
@@ -1,7 +1,7 @@
 function dropdown1() {
   if (active1 == 0) {
     document.getElementById("sub1").style.padding = "20px";
-    document.getElementById("sub1").style.height = "auto";
+    document.getElementById("sub1").style.height = "500px";
     document.getElementById("sub1").style.opacity = "1";
     document.getElementById("src1").src = "https://cdn.bjvanbemmel.nl/launchpad-preview.mp4";
     active1++;
