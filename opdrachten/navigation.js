c6c61c39d47d69ea84088d42f5cb43f55a45aa7f
bjvanbemmeml <97085143@st.deltion.nl>
2021-09-17 19:04:21 +0200

    edited CV according teacher's advice, removed shittier clone cv page on assignments

--- opdrachten/navigation.js
+++ opdrachten/navigation.js
@@ -1,6 +1,3 @@
-function gotoCV() {
-    document.getElementById("iframe").src = "./assignments/CV/";
-}
 function gotoHosting() {
     document.getElementById("iframe").src = "./assignments/Hosting/";
 }
