fa60962dc5816c11f8dd32d7cbfa094d8493c16f
bjvanbemmel <97085143@st.deltion.nl>
2022-02-20 21:46:36 +0100

    Finished both Dutch and English CV.

---
+++ dist/views/cv/cv_en.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <!-- Start of Embed -->
+    <meta content="CV - Beau Jean van Bemmel" property="og:title">
+    <meta content="My name is Beau Jean van Bemmel and I am a Software Development student @ Deltion College." property="og:description">
+    <meta content="https://bjvanbemmel.nl/cv" property="og:url">
+    <meta content="https://bjvanbemmel.nl/assets/img/portfolio.jpg" property="og:image">
+    <meta content="#36EEE0" property="theme-color">
+    <!-- End of Embed -->
+    <title>CV - Beau Jean van Bemmel</title>
+</head>
+
+<body class="p-4 flex flex-col justify-center flex-wrap items-center dark:bg-zinc-900 dark:text-zinc-200 bg-zinc-100 text-zinc-900">
+    <h1 class="text-4xl font-semibold">Curriculum Vitae</h1>
+    <section class="my-4 p-8 lg:max-w-4xl sm:max-w-2xl" id="content">
+        <div class="my-4" id="table-of-contents">
+            <h2 class="my-1 text-2xl font-semibold">Table of Contents:</h2>
+            <ul class="dark:text-zinc-400 text-zinc-700 underline">
+                <li><a href="#table-of-contents">>> Table of contents</a></li>
+                <li><a href="#contact">>> Contact information</a></li>
+                <li><a href="#profile">>> Profile</a></li>
+                <li><a href="#skills">>> Skills</a></li>
+                <li><a href="#education">>> Education</a></li>
+                <li><a href="#work">>> Work experience</a></li>
+                <li><a href="#language">>> Language</a></li>
+            </ul>
+        </div><br>
+        <div id="contact">
+            <h2 class="my-1 text-2xl font-semibold">Contact information:</h2>
+            <p><span class="font-semibold">Name: </span>Beau Jean van Bemmel</p>
+            <p><span class="font-semibold">Title: </span>Web developer</p>
+            <p><span class="font-semibold">Nationality: </span>Dutch</p>
+            <p><span class="font-semibold">Birthdate: </span>May 9th, 2003</p>
+            <p><span class="font-semibold">Personal e-mail: </span> <a class="underline" href="mailto:beauvanbemmel@live.nl">beauvanbemmel@live.nl</a></p>
+            <p><span class="font-semibold">School e-mail: </span> <a class="underline" href="mailto:97085143@st.deltion.nl">97085143@st.deltion.nl</a> <span class="font-light">[preferred for business inquiries]</span></p>
+            <p><span class="font-semibold">Mobile: </span>(+31) 0636295096</p>
+        </div><br>
+        <div id="profile">
+            <h2 class="my-1 text-2xl font-semibold">Profile:</h2>
+            <p>My name is Beau Jean van Bemmel and I am a software development student at Deltion College in Zwolle. I mostly focus on back-end web development using PHP and making CLI applications using Go. Perfectionist but flexibel, experienced with SCRUM and a quick learner.</p>
+        </div><br>
+        <div id="skills">
+            <h2 class="my-1 text-2xl font-semibold">Skills:</h2>
+            <div class="flex flex-row flex-wrap">
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">HTML</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">CSS</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Tailwind</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Bootstrap</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Javascript</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">PHP</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Go</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Shell</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">MySQL</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Linux</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Fedora</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Ubuntu</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Debian</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">OOP</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">SCRUM</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Presentations</div>
+                <div class="rounded-md bg-slate-600 text-slate-200 dark:bg-slate-200 dark:text-slate-900 px-2 py-1 mr-2 mb-2 font-semibold">Flexible</div>
+            </div>
+        </div><br>
+        <div class="my-4" id="education">
+            <h2 class="my-1 text-2xl font-semibold">Education:</h2>
+            <ul class="list-disc">
+                <li><span class="font-semibold text-lg">Software Developer</span><br><span class="text-lg">Deltion College</span> <span class="italic text-slate-600 dark:text-slate-300">[08/2021 - Current]</span><br>MBO 4<br><span class="font-semibold">Curriculum:</span>
+                    <ul class="relative ml-4 list-disc">
+                        <li>HTML & CSS</li>
+                        <li>Javascript</li>
+                        <li>PHP</li>
+                        <li>MySQL</li>
+                        <li>Linux</li>
+                        <li>Security</li>
+                    </ul>
+                </li><br>
+                <li><span class="font-semibold text-lg">Dienstverlening & Zorg</span><br><span class="text-lg">Landstede MBO</span> <span class="italic text-slate-600 dark:text-slate-300">[08/2020 - 07-2021]</span><br>MBO 1<br><span class="font-semibold">Curriculum:</span>
+                    <ul class="relative ml-4 list-disc">
+                        <li>Dutch</li>
+                        <li>Math<Tailwind/li>
+                        <li>Civics</li>
+                    </ul>
+                </li>
+            </ul>
+        </div><br>
+        <div class="my-4" id="work">
+            <h2 class="my-1 text-2xl font-semibold">Work experience:</h2>
+            <ul class="list-disc">
+                <li><span class="font-semibold text-lg">IT Assistant / Janitor [internship]</span><br><span class="text-lg">Landstede MBO</span> <span class="italic text-slate-600 dark:text-slate-300">[12/2020 - 07-2021]</span><br><span class="font-semibold">Activities:</span>
+                    <ul class="relative ml-4 list-disc">
+                        <li>Assisting teachers & students with IT-related problems</li>
+                        <li>Installing new computers within our network</li>
+                        <li>Cleaning & maintaining hallways & classrooms</li>
+                        <li>Enforcing rules</li>
+                    </ul>
+                </li><br>
+                <li><span class="font-semibold text-lg">QA Assistant [internship]</span><br><span class="text-lg">ICT-Kringloop Zwolle</span> <span class="italic text-slate-600 dark:text-slate-300">[08/2020 - 12-2020]</span><br><span class="font-semibold">Activities:</span>
+                    <ul class="relative ml-4 list-disc">
+                        <li>Testing secondhand keyboards, mice, monitors, speakers</li>
+                        <li>Cleaning secondhand keyboards, mice, monitors, PSUs</li>
+                    </ul>
+                </li>
+            </ul>
+        </div><br>
+        <div id="language">
+            <h2 class="my-1 text-2xl font-semibold">Languages:</h2>
+            <table class="table-auto text-left inline">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Reading</th>
+                        <th>Writing</th>
+                        <th>Speaking</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="w-32 font-semibold">Dutch</td>
+                        <td class="w-32">Native</td>
+                        <td class="w-32">Advanced</td>
+                        <td>Native</td>
+                    </tr>
+                    <tr>
+                        <td class="w-32 font-semibold">English</td>
+                        <td class="w-32">Advanced</td>
+                        <td class="w-32">Advanced</td>
+                        <td>Intermediate</td>
+                    </tr>
+                    <tr>
+                        <td class="font-semibold">Japanese</td>
+                        <td class="w-32">Beginner</td>
+                        <td class="w-32">Beginner</td>
+                        <td>Beginner</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+</body>
+
+</html>
