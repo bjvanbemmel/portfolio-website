47a33428615c889d189fcaf6395c4089903a555f
bjvanbemmel <97085143@st.deltion.nl>
2022-02-21 11:22:25 +0100

    Fixed link target issue, improved security by adding noreferrer noopener

--- dist/views/homepage.php
+++ dist/views/homepage.php
@@ -20,12 +20,12 @@
 <body class="flex flex-col justify-center items-center text-center dark:bg-zinc-900 dark:text-zinc-200 p-4 bg-zinc-100 text-zinc-900">
     <img class="rounded-full h-80" src="../assets/img/portfolio.jpg" alt="Picture of me">
     <h1 class="font-bold text-3xl my-2">Beau Jean van Bemmel</h1>
-    <h2>Student Software Developer @ <a target="__BLANK" href="https://ict-flex.nl/" title="Link to ICT-Flex website" class="underline">ICT-Flex</a> - <a target="__BLANK" href="https://www.deltion.nl/" title="Link to Deltion College website" class="underline">Deltion College Zwolle</a></h2>
+    <h2>Student Software Developer @ <a target="_blank" rel="noreferrer noopener" href="https://ict-flex.nl/" title="Link to ICT-Flex website" class="underline">ICT-Flex</a> - <a target="_blank" rel="noreferrer noopener" href="https://www.deltion.nl/" title="Link to Deltion College website" class="underline">Deltion College Zwolle</a></h2>
     <ul class="list-none flex flex-row">
-        <li class="m-2 underline font-semibold"><a target="__BLANK" title="My Github profile" href="https://github.com/bjvanbemmel/">Github</a></li>
-        <li class="m-2 underline font-semibold"><a target="__BLANK" title="My Twitter profile" href="https://twitter.com/BeauBemmel">Twitter</a></li>
-        <li class="m-2 underline font-semibold"><a target="__BLANK" title="My LinkedIn profile" href="https://linkedin.com/in/beau-jean-van-bemmel">LinkedIn</a></li>
-        <li class="m-2 underline font-semibold"><a target="__BLANK" title="My Curriculum Vitae" href="cv">CV</a></li>
+        <li class="m-2 underline font-semibold"><a target="_blank" rel="noreferrer noopener" title="My Github profile" href="https://github.com/bjvanbemmel/">Github</a></li>
+        <li class="m-2 underline font-semibold"><a target="_blank" rel="noreferrer noopener" title="My Twitter profile" href="https://twitter.com/BeauBemmel">Twitter</a></li>
+        <li class="m-2 underline font-semibold"><a target="_blank" rel="noreferrer noopener" title="My LinkedIn profile" href="https://linkedin.com/in/beau-jean-van-bemmel">LinkedIn</a></li>
+        <li class="m-2 underline font-semibold"><a target="_blank" rel="noreferrer noopener" title="My Curriculum Vitae" href="cv">CV</a></li>
     </ul>
 </body>
 
